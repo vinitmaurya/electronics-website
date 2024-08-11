d0f01750d1f70593f37a765844a27317b608b596
Vinit Maurya <vinitmaurya8898@gmail.com>
2024-08-11 20:41:34 +0530

    Coming Soon Default Page created successfully

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
+import { ContactComponent } from '../contact/contact.component';
+import { ProductsComponent } from '../products/products.component';
+import { DefaultComponent } from '../default/default.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "home",
+        component: HomeComponent
+    },
+    {
+        path: "about",
+        component: AboutComponent
+    },
+    {
+        path: "contact",
+        component: ContactComponent
+    },
+    {
+        path: "products",
+        component: ProductsComponent
+    },
+    {
+        path: "",
+        component: DefaultComponent
+    }
+];
