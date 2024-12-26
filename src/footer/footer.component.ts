3c42a0cb7dc7fc9c68f2ae084dff22cf1e8ad8e3
Vinit Maurya <vinitmaurya8898@gmail.com>
2024-12-26 13:48:37 +0530

    same as before

--- src/footer/footer.component.ts
+++ src/footer/footer.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../products/products.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [],
+  providers: [ProductsService],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  constructor(private productService: ProductsService) { }
 }
--- src/header/header.component.ts
+++ src/header/header.component.ts
@@ -1,20 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ProductsService } from '../products/products.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [RouterModule],
+  providers: [ProductsService],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   isMenuOpen = false;
-
+  constructor(private productService: ProductsService) { }
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
-  hideMenu(){
-    this.isMenuOpen=false;
+  hideMenu() {
+    this.isMenuOpen = false;
   }
 }
